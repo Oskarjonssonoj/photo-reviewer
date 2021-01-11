b206d52e19b0665ccc3fcb89d6d0223a28c6dd68
Oskar Jönsson <peroskarjonsson@gmail.com>
2021-01-11 16:15:18 +0100

    added functionality to sort out which album that belongs to current user

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -2,41 +2,42 @@ import React, { useState, useEffect } from 'react'
 import './styles/albums.scss'
 import {db} from '../../firebase/firebase'
 import { Switch, Route, Link } from 'react-router-dom'
+import { useAuth } from '../../contexts/ContextComp'
 
 const Albums = () => {
 
     const [albums, setAlbums] = useState([])
+    const { currentUser } = useAuth()
 
     useEffect(() => {
         const unmount = db.collection('albums').onSnapshot((snapshot) => {
             const snapAlbum = []
+            
             snapshot.forEach(doc => {
-                snapAlbum.push({...doc.data(), id: doc.id});
+                if(doc.data().owner === currentUser.uid) {
+                    snapAlbum.push({...doc.data(), id: doc.id});
+                }
             })
             setAlbums(snapAlbum)
         })
         return unmount;
     }, [])
-
     
     return (
         <div className="albums">
-
-        {
-            albums.map((album, index) => {
-
-                console.log(album)
-                return (
-                    <Link to={`/home/albums/${album.id}`}>
-                        <div className="eachAlbum" key={album.name}>
-                            <h2>{album.name}</h2>
-                            <img src={album.images ? album.images[0].url : ""}/>
-                            <p>Photos: 3</p>
-                        </div>
-                    </Link>
-                )
-            })
-        }
+            {
+                albums.map((album, index) => {
+                    return (
+                        <Link to={`/home/albums/${album.id}`}>
+                            <div className="eachAlbum" key={album.name}>
+                                <h2>{album.name}</h2>
+                                <img src={album.images ? album.images[0].url : ""}/>
+                                <p>Photos: 3</p>
+                            </div>
+                        </Link>
+                    )
+                })
+            }
         </div>
     )
 }
