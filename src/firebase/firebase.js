39f587c4b1cd7e8dc6ab597689986f05f02a2d77
Oskar Jönsson <peroskarjonsson@gmail.com>
2021-01-15 08:36:02 +0100

    added new temperary database since the last one was exceeded in memory

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/storage'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyBC12eED4eI01ByQW3_kGz6TY7SDPhKoKM",
-authDomain: "photo-reviewer-40594.firebaseapp.com",
-projectId: "photo-reviewer-40594",
-storageBucket: "photo-reviewer-40594.appspot.com",
-messagingSenderId: "120100571042",
-appId: "1:120100571042:web:a3e038076711a187fc94fb"
+apiKey: "AIzaSyBEMFd2nFpfUKh_RRXWv9DfjxpxmW39HGE",
+authDomain: "review-your-photos.firebaseapp.com",
+projectId: "review-your-photos",
+storageBucket: "review-your-photos.appspot.com",
+messagingSenderId: "527413700999",
+appId: "1:527413700999:web:fad5c01ca8b4b9d4438b22"
 };
 
 // Initialize Firebase
